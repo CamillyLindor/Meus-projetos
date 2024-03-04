8633cc244634fc2152e02796b8f3f834153fa75f
Camilly Lindor <161730513+CamillyLindor@users.noreply.github.com>
2024-03-04 14:21:20 -0300

    Update automação-PDF.py

--- projeto-automação-PDF/automação-PDF.py
+++ projeto-automação-PDF/automação-PDF.py
@@ -1,1 +1,14 @@
 #mesclando PDF
+import py PDF2
+import os
+
+mager = PyPDF2.Pdfmerger()
+
+lista_arquivos = os.listdir ('arquivos')
+Lista_arquivos.sort()
+print (lista_arquivos)
+
+for arquivos in list_arquivos:
+  in '.pdf' in arquivo:
+  merger.append(f'arquivos/{arquivos{}')
+merger.write(' PDF Final.pdf')
